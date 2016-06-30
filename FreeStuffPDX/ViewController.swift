c2bd09526bfd23f85feb18e3107128eddcbac326
ekingan <ekingan@gmail.com>
2016-06-30 15:17:57 -0700

    layout done

--- FreeStuffPDX/ViewController.swift
+++ FreeStuffPDX/ViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,13 +22,21 @@ class ViewController: UIViewController {
         imageView.image = image
         navigationItem.titleView = imageView
         
-           }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
 
+  
 
 }
 
